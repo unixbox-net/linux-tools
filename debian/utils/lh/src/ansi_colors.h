022f0280a71a70ff778dbbb3c5e04f43893da3d8
unixbox-net <104218206+unixbox-net@users.noreply.github.com>
2025-08-26 20:57:49 -0700

    Create ansi_colors.h

---
+++ debian/utils/lh/src/ansi_colors.h
@@ -0,0 +1,17 @@
+// ansi_colors.h
+#pragma once
+
+#define ANSI_COLOR_RESET      "\x1b[0m"
+#define ANSI_COLOR_RED        "\x1b[31;1m"
+#define ANSI_COLOR_GREEN      "\x1b[32;1m"
+#define ANSI_COLOR_LIGHT_GREEN "\x1b[92;1m"
+#define ANSI_COLOR_YELLOW     "\x1b[33;1m"
+#define ANSI_COLOR_BLUE       "\x1b[34;1m"
+#define ANSI_COLOR_MAGENTA    "\x1b[35;1m"
+#define ANSI_COLOR_CYAN       "\x1b[36;1m"
+#define ANSI_COLOR_WHITE      "\x1b[37;1m"
+#define ANSI_COLOR_LIGHT_GRAY "\x1b[37;1m"
+#define ANSI_COLOR_DARK       "\x1b[30m"
+#define ANSI_COLOR_DARK_GRAY  "\x1b[30;1m"
+#define ANSI_COLOR_LIGHT_BLUE "\x1b[94;1m"
+#define ANSI_COLOR_BG         "\x1b[48;5;235m"
